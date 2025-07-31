988aff29005ddec1069ce1b269b617a0b5fe9936
k1ridev <k1ri86mail@gmail.com>
2025-07-31 14:45:01 +0200

    Fix: Canonical

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,29 +1,41 @@
 import type { APIRoute } from 'astro';
 
 const pages = [
-  '',
-  '/software',
-  '/descargas',
-  '/guias',
-  '/herramientas',
-  '/es',
-  '/es/software',
-  '/es/descargas',
-  '/es/guias',
-  '/es/herramientas'
+  { path: '', priority: '1.0' },
+  { path: '/software', priority: '0.8' },
+  { path: '/descargas', priority: '0.8' },
+  { path: '/guias', priority: '0.8' },
+  { path: '/herramientas', priority: '0.8' }
 ];
 
 const baseUrl = 'https://www.codextreme.me';
 
 export const GET: APIRoute = () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(page => `  <url>
-    <loc>${baseUrl}${page}</loc>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+${pages.map(page => {
+  const englishUrl = `${baseUrl}${page.path}`;
+  const spanishUrl = `${baseUrl}/es${page.path}`;
+  
+  return `  <url>
+    <loc>${englishUrl}</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <changefreq>weekly</changefreq>
-    <priority>${page === '' ? '1.0' : '0.8'}</priority>
-  </url>`).join('\n')}
+    <priority>${page.priority}</priority>
+    <xhtml:link rel="alternate" hreflang="en" href="${englishUrl}"/>
+    <xhtml:link rel="alternate" hreflang="es" href="${spanishUrl}"/>
+    <xhtml:link rel="alternate" hreflang="x-default" href="${englishUrl}"/>
+  </url>
+  <url>
+    <loc>${spanishUrl}</loc>
+    <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>${page.priority}</priority>
+    <xhtml:link rel="alternate" hreflang="en" href="${englishUrl}"/>
+    <xhtml:link rel="alternate" hreflang="es" href="${spanishUrl}"/>
+    <xhtml:link rel="alternate" hreflang="x-default" href="${englishUrl}"/>
+  </url>`;
+}).join('\n')}
 </urlset>`;
 
   return new Response(sitemap, {
