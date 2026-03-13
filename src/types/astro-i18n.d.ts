2bc334fa8aed0662fc93f4d7d456764f3055d7d8
Kiri86Dev <kiristeam86@gmail.com>
2026-03-13 04:09:56 +0100

    chore: Remove `astro:i18n` path alias from tsconfig.json.

--- src/types/astro-i18n.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'astro:i18n' {
-
-  export function getRelativeLocaleUrl(locale: string, path?: string): string;
-
-  export function getAbsoluteLocaleUrl(locale: string, path?: string): string;
-}
